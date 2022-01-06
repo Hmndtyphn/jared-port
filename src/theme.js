3f69df5ed896278bcc9c378e6a17cd4a6c068df7
HmndTyphn <Jared.elliott09@gmail.com>
2022-01-05 18:46:25 -0600

    styling/ themes

--- src/theme.js
+++ src/theme.js
@@ -4,20 +4,28 @@
 // import { createMuiTheme } from '@mui/material/styles';
 import { createTheme } from '@mui/material/styles';
 
+// create theme function for mui styling
 const theme = createTheme({
+
+    // font sizing 
     typography: {
+        // about page name/ each pages headers
         h1: {
             fontFamily: "Permanent Marker",
         },
+
+        // about page job description
         h2: {
             fontFamily: "Permanent Marker",
         },
 
+        // about page "about me" section
         h3: {
             fontFamily: "Permanent Marker",
             fontSize: '3.4rem'
         },
 
+        // about me section body
         h4: {
             fontFamily: "Permanent Marker",
         },
@@ -33,4 +41,5 @@ const theme = createTheme({
     }
 })
 
+// export themes
 export default theme;
